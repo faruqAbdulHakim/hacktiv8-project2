bd1c900ddfbe3ff420cafa1c2279eea65e72e4ba
perdanaph <danangputro9@gmail.com>
2022-10-09 18:04:22 +0700

    update: using error middleware in router

--- routers/index.js
+++ routers/index.js
@@ -3,10 +3,13 @@ const usersRouter = require('./usersRouter');
 const photosRouter = require('./photosRouter');
 const commentsRouter = require('./commentsRouter');
 const socialMediasRouter = require('./socialmediasRouter');
+const errorMiddleware = require('./../middlewares/errorMiddleware');
 
 router.use('/users', usersRouter);
 router.use('/photos', photosRouter);
 router.use('/comments', commentsRouter);
 router.use('/socialmedias', socialMediasRouter);
+
+router.use(errorMiddleware);
 
 module.exports = router;
