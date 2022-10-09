044406b9828582137faa4e535e60df36fe311aba
faruqAbdulHakim <faruqabdulhakim2001@gmail.com>
2022-10-09 12:40:15 +0700

    add error middleware

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -5,7 +5,30 @@
  * @param {import('express').NextFunction} next
  */
 const errorMiddleware = (error, req, res, next) => {
-  // TODO: create errorMiddleware
+  let code;
+  let message;
+
+  // TODO: update errorMiddleware
+  switch (error.name) {
+    case 'JsonWebTokenError':
+      code = 401;
+      message = 'Invalid Token';
+      break;
+    case 'Invalid Token':
+      code = 401;
+      message = 'Invalid Token';
+      break;
+    case 'Unauthorized':
+      code = 401;
+      message = 'Unauthorized';
+      break;
+    default:
+      code = 500;
+      message = 'Internal Server Error';
+      break;
+  }
+
+  res.status(code).json({ message });
 };
 
 module.exports = errorMiddleware;
