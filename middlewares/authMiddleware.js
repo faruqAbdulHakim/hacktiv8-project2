1ca2fbbb492bc69903d808b6d0d46c1a62799246
perdanaph <danangputro9@gmail.com>
2022-10-09 19:04:27 +0700

    test

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -6,19 +6,19 @@ const { verify } = require('../helpers/jwtHelper');
  * @param {import('express').NextFunction} next
  */
 const authMiddleware = (req, res, next) => {
-  try {
-    const authorization = req.headers['authorization'];
-    if (!authorization) throw { name: 'Invalid Token' };
+	try {
+		const authorization = req.headers['authorization'];
+		if (!authorization) throw { name: 'Invalid Token' };
 
-    token = authorization.split(' ')[1];
-    const { id } = verify(token);
-    if (!id) throw { name: 'Unauthorized' };
+		token = authorization.split(' ')[1];
+		const { id, email } = verify(token);
+		if (!id) throw { name: 'Unauthorized' };
 
-    res.locals.user = { id };
-    next();
-  } catch (error) {
-    next(error);
-  }
+		res.locals.user = { id, email };
+		next();
+	} catch (error) {
+		next(error);
+	}
 };
 
 module.exports = authMiddleware;
