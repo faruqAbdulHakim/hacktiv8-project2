5db4f0f4fe9d5171b2386f19641cf8c631c80211
faruqAbdulHakim <faruqabdulhakim2001@gmail.com>
2022-11-14 04:41:09 +0700

    feat: update SocialMedia authoize flow

--- controllers/SocialMediaController.js
+++ controllers/SocialMediaController.js
@@ -32,9 +32,9 @@ class SocialMediaController {
    * @param {import('express').NextFunction} next
    */
   static async create(req, res, next) {
-    const { name, social_media_url } = req.body;
-    const UserId = req.user.id;
     try {
+      const { name, social_media_url } = req.body;
+      const UserId = req.user.id;
       const result = await SocialMedia.create({
         name,
         social_media_url,
@@ -52,21 +52,20 @@ class SocialMediaController {
    * @param {import('express').NextFunction} next
    */
   static async update(req, res, next) {
-    const { socialMediaId } = req.params;
-    const { name, social_media_url } = req.body;
     try {
-      // Authorization
-      const userCheck = await SocialMedia.findOne({
-        where: { id: socialMediaId },
-      });
-      if (!userCheck) throw { name: 'SocialMediaNotFound' };
-      if (userCheck.UserId != req.user.id) throw { name: 'Forbidden' };
+      const { socialMediaId } = req.params;
+      const { name, social_media_url } = req.body;
+      if (!name || !social_media_url) throw { name: 'BadRequest' };
 
-      const socialMedia = await SocialMedia.update(
+      const result = await SocialMedia.update(
         { name, social_media_url },
         { where: { id: socialMediaId }, returning: true }
       );
-      res.status(200).json({ social_media: socialMedia[1][0] });
+      if (result[0] === 0) {
+        res.status(400).json({ message: 'No Social Media updated' });
+      } else {
+        res.status(200).json({ social_media: result[1][0] });
+      }
     } catch (error) {
       next(error);
     }
@@ -78,20 +77,41 @@ class SocialMediaController {
    * @param {import('express').NextFunction} next
    */
   static async delete(req, res, next) {
-    const { socialMediaId } = req.params;
     try {
-      // Authorization
-      const userCheck = await SocialMedia.findOne({
-        where: { id: socialMediaId },
-      });
-      if (!userCheck) throw { name: 'SocialMediaNotFound' };
-      if (userCheck.UserId != req.user.id) throw { name: 'Forbidden' };
-      const socialMedia = await SocialMedia.destroy({
-        where: { id: socialMediaId },
-      });
+      const { socialMediaId } = req.params;
+      await SocialMedia.destroy({ where: { id: socialMediaId } });
       res
         .status(200)
-        .json({ message: 'Social media has been successfully deleted' });
+        .json({ message: 'Your social media has been successfully deleted' });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  /**
+   * @param {Request} req
+   * @param {Response} res
+   * @param {import('express').NextFunction} next
+   */
+  static async authorize(req, res, next) {
+    try {
+      const { socialMediaId } = req.params;
+      const socialMedia = await SocialMedia.findOne({
+        where: { id: socialMediaId },
+      });
+      if (!socialMedia) {
+        res.status(404).json({
+          name: 'Data Not Found',
+          message: `SocialMedia with id "${socialMediaId}" not found`,
+        });
+      } else if (socialMedia.UserId === req.user.id) {
+        next();
+      } else {
+        res.status(403).json({
+          name: 'Authorization Error',
+          message: `User with id "${req.user.id}" does not have permission to access SocialMedia with id "${socialMediaId}"`,
+        });
+      }
     } catch (error) {
       next(error);
     }
