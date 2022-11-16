9aced1e1ddf8c97ded39074313c46eee4815f4fb
perdanaph <danangputro9@gmail.com>
2022-11-17 00:13:23 +0700

    fixing

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const express = require('express');
 const router = require('./routers/index');
 
 const app = express();
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Welcome to myGram RESTFULL API',
+  });
+});
 app.use(router);
-
 module.exports = app;
